e03084991893eba935d8461efdd5d7ffd09a44a8
Cedric Gestes <cedric@jots.ai>
2022-11-17 16:35:19 +0100

    build faiss dependency

--- faiss-sys/build.rs
+++ faiss-sys/build.rs
@@ -1,3 +1,36 @@
+#[cfg(feature = "build")]
+use cmake;
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("cargo:rustc-link-lib=faiss_c");
+    #[cfg(feature = "build")]
+    {
+        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+        let faiss_src_dir = Path::new(&manifest_dir).join("../../faiss");
+
+        println!("Building faiss");
+
+        let build_config = &mut cmake::Config::new(faiss_src_dir);
+        build_config.very_verbose(true);
+
+        build_config.define("CMAKE_BUILD_TYPE", "Release");
+        build_config.define("FAISS_ENABLE_C_API", "ON");
+        build_config.define("FAISS_ENABLE_GPU", "OFF");
+        build_config.define("BUILD_SHARED_LIBS", "ON");
+        build_config.define("FAISS_ENABLE_PYTHON", "OFF");
+        build_config.define("BUILD_TESTING", "OFF");
+
+        let dst = build_config.build();
+        println!(
+            "cargo:rustc-link-search=native={}",
+            dst.join("build/c_api").display()
+        );
+        println!("cargo:rustc-link-lib=faiss_c");
+    }
+    #[cfg(not(feature = "build"))]
+    {
+        println!("cargo:rustc-link-lib=faiss_c");
+    }
 }
